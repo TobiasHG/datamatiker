0fdf58828bc4e09d0156b01093a38ae4b4d531b8
Tobias Harly Grünewald <harly.grunewald@gmail.com>
2026-02-22 17:09:05 +0100

    Opgave: Skridttælleren ⭐

---
+++ semester 1/Opgave Skridttælleren/Opgave Skridttælleren/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Opgave_Skridttælleren
+{
+    internal class Program
+    {
+
+        static double stringToDouble(string s)
+        {
+            s = s.Replace(".", ",");
+            return double.Parse(s);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("indtast skridt længe i cm: ");
+            double stepLenght = stringToDouble(Console.ReadLine() ?? "0.0");
+
+            Console.Write("indtast antal skridt: ");
+            double steps = stringToDouble(Console.ReadLine() ?? "0.0");
+
+            double km = (stepLenght * steps) / 100000;
+
+            Console.WriteLine($"du har gået {km}km idag");
+        }
+    }
+}
