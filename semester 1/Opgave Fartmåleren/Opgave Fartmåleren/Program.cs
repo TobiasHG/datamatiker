48d2a07eb4ef5a7223f97a8ddf1510ad1e9a6cc6
Tobias Harly Grünewald <harly.grunewald@gmail.com>
2026-02-22 17:09:36 +0100

    Opgave: Fartmåleren ⭐

---
+++ semester 1/Opgave Fartmåleren/Opgave Fartmåleren/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Opgave_Fartmåleren
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fartmåler");
+
+            Console.Write("Indtast hvor mange meter bilen har kørt: ");
+            double meters = double.Parse(Console.ReadLine() ?? "0,0");
+
+            Console.Write("Indtast hvor mange sekunder det tog: ");
+            int seconds = int.Parse( Console.ReadLine() ?? "0" );
+
+            double speed = (meters / seconds) * 3.6;
+
+            Console.WriteLine($"Bilen kørte {speed} km/t");
+
+        }
+    }
+}
