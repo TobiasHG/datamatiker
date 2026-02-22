c2b74389d95337d7c3e6300a77e94f99696af2f9
Tobias Harly Grünewald <harly.grunewald@gmail.com>
2026-02-22 21:09:25 +0100

    Opgave: Gæt en farve ⭐

---
+++ semester 1/Opgave Gæt en farve/Opgave Gæt en farve/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Opgave_Gæt_en_farve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string[] colors = { "Rød", "Blå", "Grøn", "Gul", "Lilla" };
+            ConsoleColor[] consoleColors = { ConsoleColor.Red, ConsoleColor.Blue, ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Magenta };
+
+            for (int i = 0; i < colors.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($"{i + 1}. ");
+
+                Console.ForegroundColor = consoleColors[i];
+                Console.WriteLine($"{colors[i]}");
+            }
+
+            Console.ResetColor();
+
+            Random random = new Random();
+            int chosenColorNumber = random.Next(0, 5);
+
+            int maxAttempts = 3;
+            int attempts = 0;
+
+            for (int i = 0; i < maxAttempts; i++)
+            {
+                Console.ResetColor();
+                Console.WriteLine("Gæt farven (1-5): ");
+                int userGuess = Convert.ToInt32(Console.ReadLine());
+                if (userGuess == chosenColorNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Tillykke! Du gættede rigtigt!");
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Forkert gæt. Prøv igen.");
+                    attempts++;
+                }
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
