8f1975c9b9b40071faf20bc626fc6c24c6dd9ef6
Tobias Harly Grünewald <harly.grunewald@gmail.com>
2026-02-22 18:17:28 +0100

    Opgave: Frysepunktet ⭐

---
+++ semester 1/Opgave Frysepunktet/Opgave Frysepunktet/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Opgave_Frysepunktet
+{
+    internal class Program
+    {
+
+        static bool inRange(double value, double min, double max)
+        {
+            return value >= min && value <= max;
+        }
+
+        static int calculate(double tempGoal, double tempCurrent, double tempMargin)
+        {
+            if (inRange(tempCurrent, tempGoal - tempMargin, tempGoal + tempMargin))
+            {
+                return 0;
+            }
+            else
+            {
+                if (tempCurrent < tempGoal)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+        static void Main(string[] args)
+            {
+                Console.Write("Hvad er den ønskede temp: ");
+                double tempGoal = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Hvad er den nuværende temp: ");
+                double tempCurrent = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Hvad er temp margin: ");
+                double tempMargin = Convert.ToDouble(Console.ReadLine());
+
+                double tempDiff = tempCurrent - tempGoal;
+
+                int result = calculate(tempGoal, tempCurrent, tempMargin);
+
+                if (result == 0)
+                {
+                    Console.WriteLine("Temperaturen er inden for margin");
+                }
+                else if (result == -1)
+                {
+                    Console.WriteLine("Temperaturen er for lav");
+                }
+                else if (result == 1)
+                {
+                    Console.WriteLine("Temperaturen er for høj");
+                }
+        }
+    }
+}
