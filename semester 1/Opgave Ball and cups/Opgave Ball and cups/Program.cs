498ee9db49256d62ca333a02a1684ca648752949
Tobias Harly Grünewald <harly.grunewald@gmail.com>
2026-02-22 18:25:19 +0100

    Opgave: Ball and cups ⭐

---
+++ semester 1/Opgave Ball and cups/Opgave Ball and cups/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Opgave_Ball_and_cups
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vælg, hvilket bæger du tror kuglen er i");
+            Console.WriteLine("(Tast 1, 2 eller 3 og tryk enter)");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\t#");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("\t#");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\t#\n");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\t1");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("\t2");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\t3\n");
+
+            Console.ResetColor();
+
+            Console.Write("Dit valg: ");
+
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Random r = new Random();
+            int dice = r.Next(1, 3);
+
+            if(userInput == dice)
+            {
+                Console.WriteLine("Du vandt!");
+            }
+            else
+            {
+                Console.WriteLine($"Du tabte bolden var under {dice}");
+            }
+
+        }
+    }
+}
---
+++ semester 1/Opgave Kontrolstruktur/Opgave Kontrolstruktur/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Opgave_Kontrolstruktur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] uddannelser = { "Administrationsøkonom", "Autoteknolog", "Byggetekniker", "Datamatiker" };
+
+            for (int i = 0; i < uddannelser.Length; i++)
+            {
+                Console.WriteLine($"{i}. {uddannelser[i]}");
+            }
+
+            Console.Write("\nVælg uddannelse: ");
+            string uddannelseInput = Console.ReadLine();
+
+            if (uddannelseInput == "" || uddannelseInput == null)
+            {
+                uddannelseInput = "-1";
+            }
+
+            int uddannelse = int.Parse(uddannelseInput);
+
+            if (uddannelse == -1)
+            {
+                Console.WriteLine("Du går ikke på nogen uddanelse :'(");
+            }
+            else if ( uddannelse == 3 )
+            {
+                Console.WriteLine("Du går på Datamatiker! <3");
+            }
+            else
+            {
+                Console.WriteLine("Buuuuuuh!!");
+            }
+
+        }
+    }
+}
