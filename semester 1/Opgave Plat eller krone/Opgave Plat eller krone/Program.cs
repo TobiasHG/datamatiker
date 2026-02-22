504ffcc71a598dbe432f5a0c09d569717d312196
Tobias Harly Grünewald <harly.grunewald@gmail.com>
2026-02-22 18:15:09 +0100

    Opgave: Plat eller krone ⭐

---
+++ semester 1/Opgave Plat eller krone/Opgave Plat eller krone/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Opgave_Plat_eller_krone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plat eller krone ! :)");
+
+            Random random = new Random();
+
+            int coinResult = random.Next(0, 2);
+
+            Console.WriteLine("Vælg plat eller krone (p/k) : ");
+            string myChoose = Console.ReadLine();
+
+            if (coinResult == 0)
+            {
+                Console.WriteLine("Det blev plat !");
+            }
+            else
+            {
+                Console.WriteLine("Det blev krone !");
+            }
+
+            if (coinResult == 0 && myChoose == "p")
+            {
+                Console.WriteLine("Du vandt !");
+            }
+            else if (coinResult == 1 && myChoose == "k") {
+                Console.WriteLine("Du vandt !");
+            }
+            else
+            {
+                Console.WriteLine("Du tabte !");
+            }
+
+        }
+    }
+}
