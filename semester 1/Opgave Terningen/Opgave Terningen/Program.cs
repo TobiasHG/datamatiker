8911bf5078fd7efc618dbe42c583eda8e77cc084
Tobias Harly Grünewald <harly.grunewald@gmail.com>
2026-02-22 18:16:30 +0100

    Opgave: Terningen ⭐

---
+++ semester 1/Opgave Terningen/Opgave Terningen/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Opgave_Terningen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int cubeNumber = random.Next(1, 7);
+
+            Console.WriteLine($"Du slog {cubeNumber}");
+
+        }
+    }
+}
