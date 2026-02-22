e2ae63f8ff2f4e4c588abe3c6eb06cd9d2c3d279
Tobias Harly Grünewald <harly.grunewald@gmail.com>
2026-02-22 17:18:17 +0100

    Opgave: Lottotallene ⭐

---
+++ semester 1/Opgave Lottotallene/Opgave Lottotallene/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Opgave_Lottotallene
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+
+            int[] winningNumbers = new int[7];
+            for (int i = 0; i < 7; i++)
+            {
+                winningNumbers[i] = random.Next(1, 37);
+            }
+            Array.Sort(winningNumbers);
+
+            int joker = random.Next(1, 37);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            foreach (int i in winningNumbers)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+                Thread.Sleep(2000);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(joker);
+            Console.ResetColor();
+
+        }
+    }
+}
