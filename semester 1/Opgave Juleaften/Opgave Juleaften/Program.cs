8bc4a7f4f89f4555bec1562332add530656a4e99
Tobias Harly Grünewald <harly.grunewald@gmail.com>
2026-02-22 18:07:21 +0100

    Opgave: Juleaften ⭐ ⭐

---
+++ semester 1/Opgave Juleaften/Opgave Juleaften/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Opgave_Juleaften
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;
+
+            int year = 0;
+
+            if (today.Month == 12 && today.Day > 24)
+            {
+                year = today.Year + 1;
+            }
+            else
+            {
+                year = today.Year;
+            }
+
+            DateTime christmasDate = new DateTime(year, 12, 24);
+
+            TimeSpan timeUntilChristmas = christmasDate - today;
+
+            Console.WriteLine($"Der er {timeUntilChristmas.Days} dage til juleaften.");
+        }
+    }
+}
