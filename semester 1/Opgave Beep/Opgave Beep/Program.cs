635cd5f90da442b0d99f4706bb50194e2a591282
Tobias Harly Grünewald <harly.grunewald@gmail.com>
2026-02-23 19:59:14 +0100

    Opgave : Beep ⭐

---
+++ semester 1/Opgave Beep/Opgave Beep/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Opgave_Beep
+{
+    internal class Program
+    {
+
+        static void Alarm(int times, int sleep)
+        {
+            for(int i = 0; i < times; i++)
+            {
+                Console.Beep(3276, 1000);
+                Thread.Sleep(sleep);
+                Console.Write("BEEP! ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Alarm(5, 250);
+        }
+    }
+}
