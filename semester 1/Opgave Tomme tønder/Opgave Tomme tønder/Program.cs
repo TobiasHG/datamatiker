47f6c39533acd7c0b81e9531f3f5d71837fc23c7
Tobias Harly Grünewald <harly.grunewald@gmail.com>
2026-02-23 20:18:06 +0100

    Opgave: Tomme tønder ⭐

---
+++ semester 1/Opgave Tomme tønder/Opgave Tomme tønder/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Opgave_Tomme_tønder
+{
+    internal class Program
+    {
+        static double inputToDouble(string message)
+        {
+            Console.Write(message);
+            return Convert.ToDouble(Console.ReadLine());
+
+        }
+        static double calculateVolume(double radius, double height)
+        {
+            return Math.PI * Math.Pow(radius, 2) * height;
+        }
+
+        static double calculateSurfaceArea(double radius, double height)
+        {
+            return 2 * Math.PI * Math.Pow(radius, 2) + 2 * Math.PI * radius * height;
+        }
+
+        static void Main(string[] args)
+        {
+            double height = inputToDouble("Indtast højden: ");
+            double radius = inputToDouble("Indtast radius: ");
+
+            double volume = calculateVolume(radius, height);
+            double surfaceArea = calculateSurfaceArea(radius, height);
+
+            Console.WriteLine($"Volumen: {volume}");
+            Console.WriteLine($"Overfladeareal: {surfaceArea}");
+        }
+    }
+}
