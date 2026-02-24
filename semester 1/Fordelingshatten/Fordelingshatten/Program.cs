dc3798da046c6af3f2b0b30765d05ba451af7913
Tobias Harly Grünewald <harly.grunewald@gmail.com>
2026-02-24 19:49:46 +0100

    Fordelingshatten ⭐⭐

---
+++ semester 1/Fordelingshatten/Fordelingshatten/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Fordelingshatten
+{
+    
+    internal class Program
+    {
+        private static Random rnd = new Random();
+        public enum House
+        {
+            Gryffindor, Hufflepuff, Ravenclaw, Slytherin
+        };
+
+        static House randomHouse()
+        {
+
+            House[] houses = (House[])Enum.GetValues(typeof(House));
+
+            return houses[rnd.Next(houses.Length)];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Du blev {randomHouse()}");
+        }
+    }
+}
