9d082cb08814b0e1f4d5b24e13d95c0fc45da923
Tobias Harly Grünewald <harly.grunewald@gmail.com>
2026-02-24 20:09:31 +0100

    Opgave: Superheltene ⭐

---
+++ semester 1/Opgave Superheltene/Opgave Superheltene/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Opgave_Superheltene
+{
+
+    public class SuperHero
+    {
+
+        public string name = "Unknown";
+
+        public void SuperPower()
+        {
+
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
