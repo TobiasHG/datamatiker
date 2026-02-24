8e4839eb125f045b076f1781b16ed8fad2d321ab
Tobias Harly Grünewald <harly.grunewald@gmail.com>
2026-02-24 17:13:56 +0100

    Opgave: DNA ⭐⭐

---
+++ semester 1/Opgave DNA/Opgave DNA/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Opgave_DNA
+{
+    internal class Program
+    {
+
+        static int hamming(string a, string b)
+        {
+
+            int hamming = 0;
+            int longestLength = Math.Max(a.Length, b.Length);
+            int diff = Math.Abs(a.Length - b.Length);
+
+            for(int i = 0; i < longestLength-diff; i++)
+            {
+                if (a[i] != b[i] )
+                {
+                    hamming += 1;
+                }
+            }
+            
+
+            return hamming + diff;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hamming test: ");
+            Console.WriteLine(hamming("Abc", "Abc"));       // 0
+            Console.WriteLine(hamming("Abc", "Ab"));        // 1
+            Console.WriteLine(hamming("Abc", "AbC2"));      // 2
+            Console.WriteLine(hamming("Abc", "Abcd"));      // 1
+            Console.WriteLine(hamming("Abc", "Abcd34fwa")); // 6
+        }
+    }
+}
